550b93f6a436edc1a4229924a0363c1d1fb61d9e
magikarp01 <philliphguo@gmail.com>
2025-01-02 12:06:25 +0000

    started matching sae clamping with gradient updates

--- setup_vast.sh
+++ setup_vast.sh
@@ -27,6 +27,7 @@ git config --global user.email "philliphguo@gmail.com"
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
+!pip install --upgrade sae-lens
 python -m ipykernel install --user --name=sae
 conda install -y -c conda-forge ipywidgets jupyterlab_widgets nodejs
 
